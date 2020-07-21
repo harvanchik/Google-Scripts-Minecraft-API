d6b04fb80cab225b7d20f9fa99c92f9e82af2efe
harvanchik <66141166+harvanchik@users.noreply.github.com>
2020-07-21 16:06:35 -0700

    added try/catch blocks; fixed GETUUID function name

--- code.js
+++ code.js
@@ -1,27 +1,36 @@
 /**
- * Retrieve the most recent username associated with the UUID.
- *
- * @param {string} uuid
- * @return {string} The most recent username.
- * @customfunction
- */
+* Retrieve the most recent username associated with the UUID.
+*
+* @param {string} uuid
+* @return {string} The most recent username.
+* @customfunction
+*/
 function GETIGN(uuid) {
-  var url = 'https://api.ashcon.app/mojang/v2/user/' + uuid;
-  var playerJSON = UrlFetchApp.fetch(url.toString());
-  var playerObject = JSON.parse(playerJSON);
-  return playerObject.username;
+  try {
+    var url = 'https://api.ashcon.app/mojang/v2/user/' + uuid;
+    var playerJSON = UrlFetchApp.fetch(url.toString());
+    var playerObject = JSON.parse(playerJSON);
+    return playerObject.username;
+  } catch (e) {
+    throw new Error('Enter a valid UUID')
+  }
+  
 }
 
 /**
- * Retrieve the UUID associated with the username.
- *
- * @param {string} username
- * @return {string} UUID.
- * @customfunction
- */
-function GETUSERNAME(username) {
-  var url = 'https://api.ashcon.app/mojang/v2/user/' + username;
-  var playerJSON = UrlFetchApp.fetch(url.toString());
-  var playerObject = JSON.parse(playerJSON);
-  return playerObject.uuid;
+* Retrieve the UUID associated with the username.
+*
+* @param {string} username
+* @return {string} UUID.
+* @customfunction
+*/
+function GETUUID(username) {
+  try {
+    var url = 'https://api.ashcon.app/mojang/v2/user/' + username;
+    var playerJSON = UrlFetchApp.fetch(url.toString());
+    var playerObject = JSON.parse(playerJSON);
+    return playerObject.uuid;
+  } catch (e) {
+    throw new Error('Username not found');
+  }
 }
