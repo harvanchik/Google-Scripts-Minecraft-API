0efb72c415c36a3d68273d276090271a6474a664
harvanchik <66141166+harvanchik@users.noreply.github.com>
2020-07-21 18:54:49 -0700

    added optional search UUID by date argument

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 /**
 * Retrieve the most recent username associated with the UUID.
 *
-* @param {string} uuid
+* @param {"f5d5c301-9ad3-4cb4-9cfd-a6b78e67734a"} uuid The UUID of the player.
 * @return {string} The most recent username.
 * @customfunction
 */
-function GETIGN(uuid) {
+function GETIGN(uuid, date) {
   try {
     var url = 'https://api.ashcon.app/mojang/v2/user/' + uuid;
     var playerJSON = UrlFetchApp.fetch(url.toString());
@@ -14,22 +14,27 @@ function GETIGN(uuid) {
   } catch (e) {
     throw new Error('Enter a valid UUID')
   }
-  
 }
 
 /**
 * Retrieve the UUID associated with the username.
 *
-* @param {string} username
+* @param {"Flashee"} username The username of the player.
+* @param {"7/21/2020"} date [OPTIONAL] - The time in history to search for said username.
 * @return {string} UUID.
 * @customfunction
 */
-function GETUUID(username) {
+function GETUUID(username, date) {
   try {
-    var url = 'https://api.ashcon.app/mojang/v2/user/' + username;
-    var playerJSON = UrlFetchApp.fetch(url.toString());
-    var playerObject = JSON.parse(playerJSON);
-    return playerObject.uuid;
+    if (date != null) {
+      date = new Date(date).getTime() / 1000;
+      return JSON.parse(UrlFetchApp.fetch("https://api.mojang.com/users/profiles/minecraft/" + username.toString() + "?at=" + date)).id;
+    } else {
+      var url = 'https://api.ashcon.app/mojang/v2/user/' + username;
+      var playerJSON = UrlFetchApp.fetch(url.toString());
+      var playerObject = JSON.parse(playerJSON);
+      return playerObject.uuid;
+    }
   } catch (e) {
     throw new Error('Username not found');
   }
